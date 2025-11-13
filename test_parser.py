6f10a28f96365f672dfbf36ffd0370dac48b8511
AndrewLHellman <142917243+AndrewLHellman@users.noreply.github.com>
2025-11-12 18:16:28 -0600

    Fix module name conflict

--- test_parser.py
+++ test_parser.py
@@ -1,13 +1,13 @@
 import sys
 from antlr4 import *
-from grammar.PythonSubsetLexer import PythonSubsetLexer
-from grammar.PythonSubsetParser import PythonSubsetParser
+from grammar.PythonSubsetLexer import PythonSubsetLexer as Lexer
+from grammar.PythonSubsetParser import PythonSubsetParser as Parser
 
 def main(filename):
     input_stream = FileStream(filename)
-    lexer = PythonSubsetLexer(input_stream)
+    lexer = Lexer(input_stream)
     stream = CommonTokenStream(lexer)
-    parser = PythonSubsetParser(stream)
+    parser = Parser(stream)
     tree = parser.program()  # Start rule
     print(tree.toStringTree(recog=parser))  # Print parse tree
 
@@ -16,4 +16,3 @@ if __name__ == '__main__':
         print("Usage: python test_parser.py <file.py>")
     else:
         main(sys.argv[1])
-
